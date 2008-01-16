7770297fa7dd986954fe3d3d9f7ae9b9527cc878
David Goodlad <david@goodlad.ca>
2008-01-16 12:12:38 -0800

    Cleaned and documented BitField

--- lib/bindata/bit_field.rb
+++ lib/bindata/bit_field.rb
@@ -1,8 +1,46 @@
 require 'bindata/base'
 
 module BinData
+  # A container for Bit values. It assumes that its contents will consume an
+  # even multiple of 8 bits.
+  #
+  #  bf = BitField.new(:fields => [ [:a, 4, { :initial_value => 1 }],
+  #                                 [:b, 10],
+  #                                 [:c, 2] ])
+  #  bf.a # => 4
+  #  bf.b = 0x200
+  #  io = StringIO.new
+  #  bf.write(io)
+  #  io.rewind
+  #  io.read # => "\x20\x01"
+  #
+  # A graphical representation of the bits after all that work could look
+  # like:
+  #
+  #  00100000 00000001
+  #  **
+  #    ****** ****
+  #               ****
+  #  c b          a
+  #
+  #
+  # == Assumptions
+  #
+  # The entire bit field is represented in 'big endian' form, with the
+  # most-significant byte of the entire field transmitted first. Its fields
+  # are also big-endian, as described in the description of the Bit class.
+  #
+  # == Parameters
+  #
+  # <tt>:fields</tt>:: An array of fields which make up this BitField. Each
+  #                    element of the array is of the form [name, bit length,
+  #                    params].
   class BitField < Base
     mandatory_parameter :fields
+
+    # Used by fields to store incomplete bytes left over from their read/write
+    # operations, which can subsequently be used by the following field.
+    attr_accessor :leftover_byte
 
     def initialize(params = {}, env = nil)
       super(params, env)
@@ -16,6 +54,13 @@ module BinData
       end
     end
 
+    # Allows direct access to fields' values like:
+    #
+    #  bitfield.a = 0
+    #
+    # instead of
+    #
+    #  bitfield.a.value = 0
     def method_missing(symbol, *args, &block)
       name = symbol.id2name
 
@@ -33,9 +78,9 @@ module BinData
       else
         super
       end
-
     end
 
+    # Returns the bindata object for the given field name
     def find_obj_for_name(name)
       n, o = @fields.assoc(name.to_sym)
       return o
@@ -48,8 +93,6 @@ module BinData
         find_obj_for_name(name.to_s).clear
       end
     end
-
-    attr_accessor :leftover_byte
 
     private
 
